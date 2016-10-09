cfeb72506600ec48ffac3b87f591175eca2b54f0
cdeanj <dean.chris.j@gmail.com>
2016-10-09 00:05:41 -0600

    add argument parser

---
+++ args.hpp
@@ -0,0 +1,64 @@
+#ifndef ARGS_H
+#define ARGS_H
+
+#include <string>
+#include <vector>
+#include <stdlib.h>
+
+struct cmd_args {
+	std::string ref_fp;
+	std::string annot_fp;
+	std::string sam_fp;
+	int threshold;
+	int min;
+	int max;
+	int skip;
+	int s_per_level;
+	std::string prefix;
+};
+
+static void usage() {
+        fprintf(stderr, "\n");
+        fprintf(stderr, "Program: ResistomeAnalyzer \n");
+        fprintf(stderr, "Contact: Chris Dean <cdean11@rams.colostate.edu>\n\n");
+        fprintf(stderr, "Usage: rsa [options]\n\n");
+        fprintf(stderr, "Options:\n\n");
+        fprintf(stderr, "	-ref_fp		STR/FILE		fasta file path\n");
+	fprintf(stderr, "	-annot_fp	STR/FILE		annotation file path\n");
+        fprintf(stderr, "	-sam_fp		STR/FILE		sam file path\n");
+        fprintf(stderr, "	-prefix		STR			output prefix\n\n");
+}
+
+struct cmd_args 
+inline parse_command_line(const int argc, const char *argv[]) {
+	struct cmd_args arg;
+	std::vector<std::string> args(argv, argv + argc);
+
+        for(int i = 1; i < argc; i++) {
+                if(args[i].compare("-ref_fp") == 0)
+                        arg.ref_fp = args[++i];
+		else if(args[i].compare("-annot_fp") == 0)
+			arg.annot_fp = args[++i];
+                else if(args[i].compare("-sam_fp") == 0)
+                        arg.sam_fp = args[++i];
+		else if(args[i].compare("-t") == 0)
+                        arg.threshold = atoi(args[++i].c_str());
+                else if(args[i].compare("-min") == 0)
+                        arg.min = atoi(args[++i].c_str());
+                else if(args[i].compare("-max") == 0)
+                        arg.max = atoi(args[++i].c_str());
+                else if(args[i].compare("-skip") == 0)
+                        arg.skip = atoi(args[++i].c_str());
+                else if(args[i].compare("-samples") == 0)
+                        arg.s_per_level = atoi(args[++i].c_str());
+                else if(args[i].compare("-prefix") == 0)
+                        arg.prefix = args[++i];
+                else {
+                        usage();
+                        exit(EXIT_FAILURE);
+                }
+        }
+	return arg;
+}
+
+#endif // ARGS_H
